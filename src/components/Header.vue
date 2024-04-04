def6f821b48523caa545f04be036b3796b0fcbd6
Глазунов Иван Сергеевич <glazunov-is@college.tepk.dev>
2024-04-04 11:14:31 +0700

    Заглушка Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><RouterLink to="/">Главная</RouterLink></li>
+        <li><RouterLink to="/notmain">Не сильно главная</RouterLink></li>
+        <li><RouterLink to="/reallynotmain">Прям вообще не главная</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    display: flex;
+  }
+</style>
+
+<script>
+
+</script>
